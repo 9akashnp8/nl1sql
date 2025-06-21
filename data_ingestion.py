8784e7edfed8e5aa30269b75eb4be5ba019ffd02
Akash NP <akashnp1998@gmail.com>
2025-06-21 14:31:42 +0400

    chore: minor changes

--- data_ingestion.py
+++ data_ingestion.py
@@ -2,7 +2,6 @@ import psycopg
 from typing import List
 
 from vectorstore import vectorize_documents
-from constants import abu_dhabi_entities
 
 def ingest_data(db_connection_url, table_name: str, columns: List[str]) -> None:
 
@@ -11,7 +10,7 @@ def ingest_data(db_connection_url, table_name: str, columns: List[str]) -> None:
         with psycopg.connect(db_connection_url) as conn:
             with conn.cursor() as cursor:
                 for column in columns:
-                    cursor.execute(f'SELECT DISTINCT "{column}" FROM {table_name}') # assuming public schema
+                    cursor.execute(f'SELECT DISTINCT "{column}" FROM {table_name} LIMIT 100') # assuming public schema
                     data = cursor.fetchall()
                     data = [row[0] for row in data if row[0] is not None]
                     vectorize_documents(column, data)
