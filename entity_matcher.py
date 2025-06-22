744eaf3722574687bbde880d6c0488dddcd8e068
Akash NP <akashnp1998@gmail.com>
2025-06-22 17:44:08 +0400

    chore: refactor entity matching funcs into class
    + remove threshold filtering for fuzzy search, similar to semantic search

--- entity_matcher.py
+++ entity_matcher.py
@@ -1,21 +1,33 @@
 import os
 import psycopg
-from typing import List
+from typing import Tuple, List
 from vectorstore import query
 
 
-def fuzzy_matcher(table_name: str, column_name: str, value: str) -> List[str]:
-    """check if fuzzy match exists in the table"""
-    with psycopg.connect(os.getenv("DB_CONNECTION_STRING")) as conn:
-        with conn.cursor() as cur:
-            query = f"SELECT distinct {column_name} FROM {table_name} WHERE SIMILARITY({column_name}, '{value}') > 0.3 LIMIT 5"
-            print(query)
-            cur.execute(query)
-            exists = cur.fetchall()
-            return [row[0] for row in exists] if exists else []
+class EntityMatcher:
 
+    def __init__(self, table_name: str, column_name: str):
+        self.table_name = table_name
+        self.column_name = column_name
 
-def semantic_search(column_name: str, value: str) -> bool:
-    """check if semantic search match exists in the table"""
-    result = query(f"{column_name}", value, 5)
-    return result
+    def fuzzy_match(self, value: str) -> List[Tuple[str, float]]:
+        """check if fuzzy match exists in the table"""
+        with psycopg.connect(os.getenv("DB_CONNECTION_STRING")) as conn:
+            with conn.cursor() as cur:
+                query = f"""
+                    SELECT *
+                    FROM (
+                        SELECT {self.column_name}, similarity({self.column_name}, '{value}') AS score
+                        FROM {self.table_name}
+                    ) sub
+                    ORDER BY score DESC
+                    LIMIT 10;
+                """
+                cur.execute(query)
+                exists = cur.fetchall()
+                return [(row[0], row[1]) for row in exists] if exists else []
+
+    def semantic_search(self, value: str) -> bool:
+        """check if semantic search match exists in the table"""
+        result = query(f"{self.column_name}", value, 10)
+        return result
